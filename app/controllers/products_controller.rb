b6056655ae97e8a1681ca6d1dffb46f4ed135483
Michael Tuhaimer <michaeltuhaimer@Michaels-Air.localdomain>
2024-07-10 12:47:46 -0500

    Updated jbuilder files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   def one_product
-    product = Product.find_by(id: 1)
-    render json: { product: product }
+    @product = Product.find_by(id: 1)
+    render template: "products/show"
   end
 
   def all_products
-    products = Product.all
-    render json: { products: products }
+    @products = Product.all
+    render template: "products/index"
   end
 end
