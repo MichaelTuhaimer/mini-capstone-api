c4c44c9444f6650b7dcf625c766bc5d55501d2c2
Michael Tuhaimer <michaeltuhaimer@Michaels-MacBook-Air.local>
2024-07-30 22:34:58 -0500

    Add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 
   get "/products/:id" => "products#show"
   get "/products" => "products#index"
